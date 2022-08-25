1de1b6db64a6d8d3f252b8025d15a52794322cd9
Debendra Bakhati <debendrabakhati762@gmail.com>
2022-08-25 20:03:34 +0545

    class 8 updated

--- basic_java_project/src/com/Basic_java_class_8/ReturnStatementDemo.java
+++ basic_java_project/src/com/Basic_java_class_8/ReturnStatementDemo.java
@@ -1,12 +1,21 @@
 package com.Basic_java_class_8;
 
+import java.util.Scanner;
+
 public class ReturnStatementDemo {
-	int sum_1(int num_1) {
-		int sum_1;
-		 sum_1=sum_1+num_1;
-				return 0;
-	}
+
 	public static void main(String[] args) {
-		
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int dataInt=scanner.nextInt();
+		int sum=sum_1(dataInt);
+		System.out.println("The return sum is :"+sum);
+		scanner.close();
+
+	}
+	public static int sum_1(int num_1) {
+		int sum=1;
+		sum=sum+num_1;
+		return sum;
 	}
 }
